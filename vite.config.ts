16e739db59650ec1ce75090a144818cfc98bdf05
Learnify-Tech <afellah626@gmail.com>
2026-06-20 13:51:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          gsap: ['gsap'],
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['gsap', 'lenis'],
+  },
+});
